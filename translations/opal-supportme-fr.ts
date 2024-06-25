82fcb0b98c84e1da62304a5ff6a275e3286b5db6
Languages add-on <noreply-addon-languages@weblate.org>
2024-06-25 13:33:17 +0200

    Added translation using Weblate (Norwegian Bokmål)
    Added translation using Weblate (Chinese (Simplified))
    
    Added translation using Weblate (Slovak)
    
    Added translation using Weblate (Finnish)
    
    Added translation using Weblate (Swedish)
    
    Added translation using Weblate (French)
    
    Added translation using Weblate (Spanish)
    
    Added translation using Weblate (Dutch)
    
    Added translation using Weblate (Indonesian)
    
    Added translation using Weblate (Italian)
    
    Added translation using Weblate (Polish)
    
    Added translation using Weblate (Hungarian)
    
    Added translation using Weblate (Czech)
    
    Added translation using Weblate (Ukrainian)
    
    Added translation using Weblate (Russian)
    
    Added translation using Weblate (Estonian)
    
    Added translation using Weblate (English)
    
    Added translation using Weblate (Turkish)
    
    Co-authored-by: Languages add-on <noreply-addon-languages@weblate.org>

---
+++ translations/opal-supportme-fr.ts
@@ -0,0 +1,81 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>AskForSupport</name>
+    <message>
+        <location filename="../Opal/SupportMe/AskForSupport.qml" line="160"/>
+        <source>Please take a moment to consider if you can contribute to this project in one way or another.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DetailsDrawer</name>
+    <message>
+        <location filename="../Opal/SupportMe/DetailsDrawer.qml" line="208"/>
+        <source>show less</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/DetailsDrawer.qml" line="209"/>
+        <source>show more</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Opal.LinkHandler</name>
+    <message>
+        <location filename="../Opal/SupportMe/private/LinkHandler/private/ExternalUrlPage.qml" line="27"/>
+        <source>External Link</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/LinkHandler/private/ExternalUrlPage.qml" line="59"/>
+        <source>Open in browser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/LinkHandler/private/ExternalUrlPage.qml" line="60"/>
+        <source>Open externally</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/LinkHandler/private/ExternalUrlPage.qml" line="70"/>
+        <source>Copied to clipboard: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/LinkHandler/private/ExternalUrlPage.qml" line="79"/>
+        <source>Copy to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SupportDialog</name>
+    <message>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="40"/>
+        <source>Hi there!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="51"/>
+        <source>Thank you for using my little app! Maybe you can contribute back?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="62"/>
+        <source>Thank you for your support!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="152"/>
+        <source>Remind me later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="157"/>
+        <source>Don&apos;t ask me again</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
