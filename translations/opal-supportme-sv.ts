a94ad6ec00ef4075223d41f1eafe7bab7e7755aa
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2024-06-25 13:35:54 +0200

    Update translations

--- translations/opal-supportme-sv.ts
+++ translations/opal-supportme-sv.ts
@@ -51,6 +51,69 @@
     </message>
 </context>
 <context>
+    <name>Opal.SupportMe.Common</name>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="12"/>
+        <source>Donate on Liberapay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="13"/>
+        <source>Pay the amount of a cup of coffee, a slice of pizza, or a ticket to the theater.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="16"/>
+        <source>Translate on Weblate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="17"/>
+        <source>Help with translating this app in as many languages as possible.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="20"/>
+        <source>Develop on Github</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="21"/>
+        <source>Support with maintenance and packaging, write code, or provide valuable bug reports.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="24"/>
+        <source>Why should you care?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="25"/>
+        <source>This project is built with love and passion by a single developer in their spare time, and is provided to you free of charge.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="29"/>
+        <source>I develop Free Software because I am convinced that it is the ethical thing to do - and it is a fun hobby. However, developing software takes a lot of time and effort. As Sailfish and living in general costs money, I need your support to be able to spend time on non-paying projects like this.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="36"/>
+        <source>Why donate?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="37"/>
+        <source>Jolla raised prices and is trying to force developers (who work for free) to pay rent for Sailfish.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="40"/>
+        <source>If you can afford it, donating is the easiest way to ensure that I can continue working on apps for Sailfish. Any amount is appreciated, be it a cup of coffee, a slice of pizza, or more.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>SupportDialog</name>
     <message>
         <location filename="../Opal/SupportMe/SupportDialog.qml" line="40"/>
