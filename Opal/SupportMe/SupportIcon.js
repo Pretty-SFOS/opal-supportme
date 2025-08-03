e665e06247ce820a2111f4c7f74d1163b67128e9
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-08-03 16:33:26 +0200

    Fix SupportIcon documentation

--- Opal/SupportMe/SupportIcon.js
+++ Opal/SupportMe/SupportIcon.js
@@ -9,6 +9,7 @@
     \qmltype SupportIcon
     \inqmlmodule Opal.SupportMe
     \inherits QtObject
+
     \brief Enum of icons for common support actions.
 
     Please contribute your own service icons so they
@@ -16,33 +17,39 @@
 */
 
 /*!
-  Liberapay: liberapay.com
+  \qmlproperty url SupportIcon::Liberapay
+
+  \tt{ \b liberapay.com }
 
   Service for recurring or one-time donations.
 
-  Source: https://liberapay.com/about/logos
+  Source: \l https://liberapay.com/about/logos
 
   Licensed under CC0-1.0.
 */
 var Liberapay = Qt.resolvedUrl("private/icons/liberapay.png")
 
 /*!
-  Weblate: weblate.org
+  \qmlproperty url SupportIcon::Weblate
+
+  \tt{ \b weblate.org }
 
   Service for crowdsourced translations.
 
-  Source: https://github.com/WeblateOrg/graphics/blob/main/logo/weblate-minborder.svg
+  Source: \l https://github.com/WeblateOrg/graphics/blob/main/logo/weblate-minborder.svg
 */
 var Weblate = Qt.resolvedUrl("private/icons/weblate.png")
 
 /*!
-  Git: git-scm.org
+  \qmlproperty url SupportIcon::Git
+
+  \tt{ \b git-scm.org }
 
   Version control system. Use this icon if you are hosting
   your repositories publicly (e.g. on Gitlab, Github,
   Sourcehut, Codeberg, Bitbucket, etc.).
 
-  Source: https://git-scm.com/downloads/logos
+  Source: \l https://git-scm.com/downloads/logos
 
   Git Logo by Jason Long is licensed under the
   Creative Commons Attribution 3.0 Unported License.
