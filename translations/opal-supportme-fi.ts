9258eef77b9a891940f639160d41b1e54447cbd0
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2025-11-26 20:40:44 +0100

    Update translations

--- translations/opal-supportme-fi.ts
+++ translations/opal-supportme-fi.ts
@@ -4,7 +4,7 @@
 <context>
     <name>AskForSupport</name>
     <message>
-        <location filename="../Opal/SupportMe/AskForSupport.qml" line="181"></location>
+        <location filename="../Opal/SupportMe/AskForSupport.qml" line="181"/>
         <source>Please take a moment to consider if you can contribute to this project in one way or another.</source>
         <translation>Harkitse, voisitko tukea tätä projektia jollain tavalla.</translation>
     </message>
@@ -12,12 +12,12 @@
 <context>
     <name>DetailsDrawer</name>
     <message>
-        <location filename="../Opal/SupportMe/DetailsDrawer.qml" line="209"></location>
+        <location filename="../Opal/SupportMe/DetailsDrawer.qml" line="209"/>
         <source>show less</source>
         <translation>näytä vähemmän</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/DetailsDrawer.qml" line="210"></location>
+        <location filename="../Opal/SupportMe/DetailsDrawer.qml" line="210"/>
         <source>show more</source>
         <translation>näytä enemmän</translation>
     </message>
@@ -25,62 +25,62 @@
 <context>
     <name>MySupportDialog</name>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="12"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="12"/>
         <source>Donate on Liberapay</source>
         <translation>Lahjoita Liberapayssä</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="13"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="13"/>
         <source>Pay the amount of a cup of coffee, a slice of pizza, or a ticket to the theater.</source>
         <translation>Maksa kahvikupin, pizzasiivun tai elokuvalipun verran.</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="16"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="16"/>
         <source>Translate on Weblate</source>
         <translation>Käännä Weblatessa</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="17"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="17"/>
         <source>Help with translating this app in as many languages as possible.</source>
         <translation>Auta kääntämään tämä sovellus mahdollisimman monelle kielelle.</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="20"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="20"/>
         <source>Develop on Github</source>
         <translation>Kehitä GitHubissa</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="21"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="21"/>
         <source>Support with maintenance and packaging, write code, or provide valuable bug reports.</source>
         <translation>Auta ylläpidossa ja paketoinnissa, kirjoita koodia tai jaa arvokkaita vikaraportteja.</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="24"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="24"/>
         <source>Why should you care?</source>
         <translation>Miksi välittäisit?</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="25"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="25"/>
         <source>This project is built with love and passion by a single developer in their spare time, and is provided to you free of charge.</source>
         <translation>Tämä on yhden kehittäjän vapaa-ajallaan rakkaudella ja intohimolla rakentama projekti ja se tarjotaan sinulle ilmaiseksi.</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="29"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="29"/>
         <source>I develop Free Software because I am convinced that it is the ethical thing to do - and it is a fun hobby. However, developing software takes a lot of time and effort. As Sailfish and living in general costs money, I need your support to be able to spend time on non-paying projects like this.</source>
         <translation>Kehitän ilmaisia ohjelmistoja, koska uskon, että se on eettinen tapa toimia - se on myös hauska harrastus. Ohjelmistojen kehittäminen vaatii kuitenkin paljon aikaa ja vaivaa. Koska Sailfish sekä eläminen yleensäkin maksaa, tarvitsen tukeasi, jotta voin käyttää aikaani tämän kaltaisiin palkattomiin projekteihin.</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="36"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="36"/>
         <source>Why donate?</source>
         <translation>Miksi lahjoittaa?</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="37"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="37"/>
         <source>Jolla raised prices and is trying to force developers (who work for free) to pay rent for Sailfish.</source>
         <translation>Jolla korotti hintojaan ja yrittää saada kehittäjät (jotka työskentelevät palkatta) maksamaan vuokraa Sailfishistä.</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="40"></location>
+        <location filename="../Opal/SupportMe/private/ExtraTranslations.qml" line="40"/>
         <source>If you can afford it, donating is the easiest way to ensure that I can continue working on apps for Sailfish. Any amount is appreciated, be it a cup of coffee, a slice of pizza, or more.</source>
         <translation>Jos sinulla on mahdollisuus lahjoittaa, se on helpoin tapa varmistaa, että voin jatkaa sovellusten kehittämistä Sailfishille. Mikä tahansa summa on tervetullut, oli se sitten kupillinen kahvia, pala pizzaa tai enemmän.</translation>
     </message>
@@ -88,27 +88,27 @@
 <context>
     <name>SupportDialog</name>
     <message>
-        <location filename="../Opal/SupportMe/SupportDialog.qml" line="40"></location>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="40"/>
         <source>Hi there!</source>
         <translation>Hei siellä!</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/SupportDialog.qml" line="51"></location>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="51"/>
         <source>Thank you for using my little app! Maybe you can contribute back?</source>
         <translation>Kiitos, että kokeilit sovellustani! Ehkä voisit tukea tätä projektia?</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/SupportDialog.qml" line="62"></location>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="62"/>
         <source>Thank you for your support!</source>
         <translation>Kiitos tuestasi!</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/SupportDialog.qml" line="148"></location>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="148"/>
         <source>Remind me later</source>
         <translation>Muistuta myöhemmin</translation>
     </message>
     <message>
-        <location filename="../Opal/SupportMe/SupportDialog.qml" line="153"></location>
+        <location filename="../Opal/SupportMe/SupportDialog.qml" line="153"/>
         <source>Don&apos;t ask me again</source>
         <translation>Älä kysy uudelleen</translation>
     </message>
